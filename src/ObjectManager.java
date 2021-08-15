925e388e9dc02205c45ff7d397268ed620975314
devikas48 <51810081+devikas48@users.noreply.github.com>
2021-08-14 17:57:05 -0700

    model management step 5

--- src/ObjectManager.java
+++ src/ObjectManager.java
@@ -1,13 +1,44 @@
 import java.util.ArrayList;
+import java.util.Random;
 
 public class ObjectManager {
 
 	Frog frog;
 	ArrayList<Log> logs = new ArrayList<Log>();
 	ArrayList<Car> cars = new ArrayList<Car>();
-
+	Random random = new Random();
+	
 	public ObjectManager(Frog frog) {
 		this.frog = frog;
 
+	
 	}
+	
+
+	public void addLog() {
+		logs.add(new Log(random.nextInt(Frogger.width),0,50,50));
+	
+	}
+	
+	public void update() {
+		for (int i = 0; i < logs.size(); i++) {
+			logs.get(i).update();
+		
+			
+			if(logs.get(i).y > Frogger.height) {
+				logs.get(i).isActive = false;
+			}
+		}
+		
+		for (int i = 0; i < cars.size(); i++) {
+			cars.get(i).update();
+		
+			if(logs.get(i).y > Frogger.height) {
+				logs.get(i).isActive = false;
+			}
+			
+			}
+		}
+	}
+	
 }
